8aeaa50b4ea9bec478e50733d25277a5039299d4
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-01 13:05:54 +0300

    added create(), modifided main(), added destroy()

--- main.cpp
+++ main.cpp
@@ -1,2 +1,39 @@
+#include <iostream>
+
+void destroy(int ** mtx, size_t created);
+int ** create(size_t rows, size_t cols);
+
 int main()
-{}
+{
+  int ** matrix = nullptr;
+  try {
+    matrix = create(5, 5);
+  } catch (const std::bad_alloc & e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+  destroy(matrix, 5);
+}
+
+void destroy(int ** mtx, size_t created)
+{
+  for (size_t i = 0; i < created; ++i) {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+
+int ** create(size_t rows, size_t cols)
+{
+  int ** mtx = new int * [rows];
+  size_t created = 0;
+  try {
+    for (; created < rows; ++created) {
+      mtx[created] = new int[cols];
+    } 
+  } catch (const std::bad_alloc & e) {
+    destroy(mtx, created);
+    throw;
+  }
+  return mtx;
+}
